bbfa10813b279cdbe7a0e3fe0e7f22d12a57b381
Flamesone2 <sergbasov.com@gmail.com>
2024-11-26 19:30:44 +0300

    added create endpoint

--- DjangoWebsitePGpowered/todo_list/urls.py
+++ DjangoWebsitePGpowered/todo_list/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+
+from .forms import ToDoItemForm
 from .views import (index_view,
                     ToDoListIndexView,
                     ToDoListView,
                     ToDoListDoneView,
-                    ToDoDetailView)
+                    ToDoDetailView, ToDoItemCreateView)
 app_name = "todo_list"
 
 urlpatterns = [
@@ -14,5 +16,5 @@ urlpatterns = [
     path("<int:pk>/", ToDoDetailView.as_view(), name="detail"),
     path("list/", ToDoListView.as_view(), name="list"),
     path("done/", ToDoListDoneView.as_view(), name="done"),
-
+    path("create/", ToDoItemCreateView.as_view(), name="create"),
 ]
