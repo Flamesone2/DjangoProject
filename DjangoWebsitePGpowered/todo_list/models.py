2107e41bb9d4505ab964d3d466c7d004c4b06b80
Flamesone2 <sergbasov.com@gmail.com>
2024-11-26 19:30:07 +0300

    added get_absolute_url method

--- DjangoWebsitePGpowered/todo_list/models.py
+++ DjangoWebsitePGpowered/todo_list/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 
 class ToDoItem(models.Model):
 
@@ -7,6 +7,12 @@ class ToDoItem(models.Model):
         ordering = ("id",)
         verbose_name = 'ToDo Item'
     title = models.CharField(max_length=250)
+    description = models.TextField(blank=True, null=False)
     done = models.BooleanField(default=False)
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("todo_list:detail",
+                kwargs={"pk": self.pk},
+                )
